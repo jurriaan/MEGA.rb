c9cdcdcfa224b066b7457228f52f167e901ceeb5
Jurriaan Pruis <email@jurriaanpruis.nl>
2013-01-26 13:27:26 +0100

    Fixed some minor mistakes

--- lib/mega/crypto.rb
+++ lib/mega/crypto.rb
@@ -42,51 +42,72 @@ module MEGA
       
       @@cipher = OpenSSL::Cipher::AES.new(128, :ECB)
       
-      def get_cipher(key)
+      def get_cipher(key, decrypt = false)
         packing = 'l>*'
         @@cipher.reset
-        @@cipher.encrypt
-        @@cipher.key=key.pack(packing)
+        @@cipher.padding=0
+        if decrypt
+          @@cipher.decrypt 
+        else
+          @@cipher.encrypt
+        end
+        if key.is_a? String
+          @@cipher.key = key
+        else 
+          @@cipher.key = key.pack(packing)
+        end
         @@cipher
       end
       
       # Does not work yet :(, need some help figuring out how RSA encryption works and how to parse the private key so OpenSSL can use it
       def decrypt_key(key, real_key)
+         @@cipher = OpenSSL::Cipher::AES.new(128, :ECB)
         packing = 'l>*'
-        cipher = get_cipher(key) 
-        
-	      if real_key.length == 4 
-          return cipher.update(real_key.pack(packing)).unpack(packing)
-        end
-        x = []
-        
-        0.step(real_key.length-1, 4) do |i|
-          cipher = get_cipher(key) 
-          x += cipher.update([real_key[i],real_key[i+1],real_key[i+2],real_key[i+3]].pack(packing)).unpack(packing)
-        end
-        x
+        #p "key = #{key}, real_key = #{real_key} "
+        cipher = get_cipher(key, true)
+        real_key = real_key.pack(packing) unless real_key.is_a? String
+        (cipher.update(real_key)+cipher.final).unpack(packing)
       end
 
       # Does not work yet.. :(
       
       def decrypt_session_id key, csid, privk, password_key
+        p key
   			key = MEGA::Utils.base64_to_a32(key)
-  			if key.length == 4
+        p key
+  			if key.size == 4
+
+          p password_key
+          p key
           key = decrypt_key(password_key,key)
-          p csid
-          
+          p "Decrypted key: #{key}"
+          #p csid
           privk = MEGA::Utils.a32_to_str(decrypt_key(key,MEGA::Utils.base64_to_a32(privk)))
-          privbytes = privk.bytes.to_a
+          p privk.bytesize
+          p Base64.encode64(privk)
           
+          #puts privk
+          privbytes = privk.bytes.to_a
+        
+          open('test.key','w') do |f|
+            f << privk
+          end
+          
+          #key = OpenSSL::PKey::RSA.new privk
+          
+          #p key
+         # p privbytes.collect {|a| a.to_s(16)}.join(' ').upcase
           #t = mbi2b(Base64.decode64(csid))
+          
           rsa_privk = [0,0,0,0]
           4.times do |i|
             l = ((privbytes[0]*256+privbytes[1] + 7)>>3)+2
-            rsa_privk[i] = mpi2b(priv.byteslice(0..l))
+            rsa_privk[i] = mpi2b(privk.byteslice(0..l))
             #if (typeof rsa_privk[i] == 'number') break;
-            privk= priv.byteslice(l..-1)  #privk = privk.substr(l);
+            privk= privk.byteslice(l..-1)  #privk = privk.substr(l);
           end
           p rsa_privk
+          raise
           if privk.bytesize > 16
             #(m, d, p, q, u)
             return [k,Base64.encode64(MEGA::Utils.b2s(RSAdecrypt(t,rsa_privk[2],rsa_privk[0],rsa_privk[1],rsa_privk[3])).substr(0,43)),rsa_privk]
--- lib/mega/utils.rb
+++ lib/mega/utils.rb
@@ -2,7 +2,7 @@ module MEGA
   class Utils
     class << self
       def padstr str
-        str.ljust(str.length + 4 - (str.length % 4), "\x00")
+        str.ljust(str.bytesize + ((str.bytesize) % 4), "\x00")
       end
   
       def str_to_a32 str
@@ -13,8 +13,14 @@ module MEGA
         (str).pack('l>*')
       end
       
+      def base64urldecode(s) 
+        s = s+'='*((s.bytesize*3)&3)
+        s = s.gsub(/\-/,'+').gsub(/_/,'/').gsub(/,/,'')
+        Base64.decode64(s)
+      end
+      
       def base64_to_a32(s)
-        str_to_a32(Base64.decode64(s))
+        str_to_a32(base64urldecode(s))
       end
       
       def b2s barr
